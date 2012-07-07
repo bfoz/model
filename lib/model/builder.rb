6ffda89bd8b1a6612c78cdabeba8ed81e3f4a129
Brandon Fosdick <bfoz@bfoz.net>
2012-07-07 16:09:18 -0700

    Builder#extrude accepts a block and applies it to the Extrusion's sketch

--- lib/model/builder.rb
+++ lib/model/builder.rb
@@ -13,8 +13,11 @@ class Model
 	# Create and add an {Extrusion} object with the given length and {Sketch}
 	# @param [Numeric]	length	The length of the {Extrusion}
 	# @param [Sketch]	sketch	The {Sketch} to extrude
+	# @param [Block]	block	A block to use for creating, or modifying, the {Sketch} to extrude
 	# @return [Extrusion]   A new {Extrusion}
-	def extrude(length, sketch)
+	def extrude(length, sketch=nil, &block)
+	    sketch = Sketch.new unless sketch
+	    sketch.instance_eval(&block) if block_given?
 	    @model.add_extrusion Extrusion.new(length, sketch)
 	end
     end
--- test/model/builder.rb
+++ test/model/builder.rb
@@ -26,4 +26,42 @@ describe Model::Builder do
 	    @builder.model.elements.last.must_be_instance_of Model::Extrusion
 	end
     end
+
+    describe "when adding an Extrusion with a length and a Sketch" do
+	before do
+	    @builder.extrude 10, Sketch.new do
+		add_rectangle 5, 6
+	    end
+	    @builder.model.elements.last.must_be_instance_of Model::Extrusion
+	end
+
+	it "should have an Extrusion element" do
+	    @builder.model.elements.last.must_be_instance_of Model::Extrusion
+	end
+
+	it "should make a Rectangle in the Extrusion's Sketch" do
+	    extrusion = @builder.model.elements.last
+	    sketch = extrusion.sketch
+	    sketch.elements.last.must_be_kind_of Geometry::Rectangle
+	end
+    end
+
+    describe "when adding an Extrusion with a length and a block" do
+	before do
+	    @builder.extrude 11 do
+		add_rectangle 5, 6
+	    end
+	end
+
+	it "should have an Extrusion element" do
+	    @builder.model.elements.last.must_be_instance_of Model::Extrusion
+	end
+
+	it "should make a Rectangle in the Extrusion's Sketch" do
+	    extrusion = @builder.model.elements.last
+	    sketch = extrusion.sketch
+	    sketch.elements.last.must_be_kind_of Geometry::Rectangle
+	end
+    end
+
 end

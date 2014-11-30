fbc18ee7f99f049cea3c65bafdf8e4d9947aeacb
Brandon Fosdick <bfoz@bfoz.net>
2014-11-29 23:35:12 -0800

    Minor spec cleanup

--- test/model/extrusion.rb
+++ test/model/extrusion.rb
@@ -31,7 +31,7 @@ describe Model::Extrusion do
     end
 
     describe "properties" do
-	let(:model) do
+	let(:extrusion) do
 	    Extrusion.new(length:5, sketch:Sketch.new.tap(){|sketch| sketch.push Geometry::Rectangle.new(center:[0,0], size:[10,10])})
 	end
 
@@ -40,19 +40,19 @@ describe Model::Extrusion do
 	end
 
 	it 'must have a max property that returns the upper-right-back corner of the bounding box' do
-	    model.max.must_equal Point[5.0,5.0,5.0]
+	    extrusion.max.must_equal Point[5.0,5.0,5.0]
 	end
 
 	it 'must have a min property that returns the lower-left-front corner of the bounding box' do
-	    model.min.must_equal Point[-5.0,-5.0,0.0]
+	    extrusion.min.must_equal Point[-5.0,-5.0,0.0]
 	end
 
 	it 'must have a minmax property that returns the corners of the bounding rectangle' do
-	    model.minmax.must_equal [Point[-5.0,-5.0,0.0], Point[5.0,5.0,5.0]]
+	    extrusion.minmax.must_equal [Point[-5.0,-5.0,0.0], Point[5.0,5.0,5.0]]
 	end
 
 	it 'must have a size' do
-	    model.size.must_equal Geometry::Size[10,10,5]
+	    extrusion.size.must_equal Geometry::Size[10,10,5]
 	end
     end
 
